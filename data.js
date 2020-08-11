7ff13bdec2a5dbaf2912fb68ea3d0618f457fedd
nguyenchicong1995 <69504809+nguyenchicong1995@users.noreply.github.com>
2020-08-11 15:16:03 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,8 @@
-//
+function getData(){
+	var dataAMZ = {
+	asin1: {US: "google.com",CA: "",UK: ""},
+	asin2: {US: "fb.com",CA: "",UK: ""},
+	asin3: {US: "zalo.com",CA: "",UK: ""}
+	}
+	return dataAMZ;
+}
